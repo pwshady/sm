80c600e3fad88c7186de6d2235a39ec32bab550d
Pwshady <pwshady@gmail.com>
2023-08-04 23:42:24 +0300

    040823

--- app/widgets/baseauthdb/loginView.php
+++ app/widgets/baseauthdb/loginView.php
@@ -1,32 +1,30 @@
-<div class="content">
-    <div class="container">
-        <h1>Личный кабинет</h1>
-        <div class="form-section">
-            <div class="form-content -sign">
-                <div class="input-item">
+
+
+<div class="<?= $this->prefix_kebab ?>div">
+<div class="<?= $this->prefix_kebab ?>content">
+    <div class="<?= $this->prefix_kebab ?>container">
+        <div class="<?= $this->prefix_kebab ?>form-section">
+            <div class="<?= $this->prefix_kebab ?>form-content-sign">
+                <div class="<?= $this->prefix_kebab ?>input-item">
                     <div class="-label"></div>
                     <div class="-input">
-                        <ul class="tabs">
-                            <li class="tabs-item tabs-item-active">Войти</li>
-                            <li><a href="https://www.promkod.ru/register" class="tabs-item -links">Зарегистрироваться</a></li>
+                        <ul class="-tabs">
+                            <li class="-tabs-item -tabs-item-active">Войти</li>
+                            <li><a href="https://www.promkod.ru/register" class="-tabs-item -links">Зарегистрироваться</a></li>
                         </ul>
                     </div>
                 </div>
                 <form method="POST" action="https://www.promkod.ru/login">
-                    <input type="hidden" name="_token" value="Y4PgzoPPsP1ogoIV2jOJvq1kYWHDpxtyB0bIeI0t">
-                    <input type="hidden" name="previousUrl" value="">
-                    <div class="input-item">
+                    <div class="<?= $this->prefix_kebab ?>input-item">
                         <div class="-label">
                             <label for="email">Электронная почта</label>
                         </div>
                         <div class="-input">
                             <input id="email" type="email" name="email" value="" >
-                            <div class="validation-error">Введите e-mail</div>
+                            <div class="-validation-error">Введите e-mail</div>
                         </div>
-
-
                     </div>
-                    <div class="input-item">
+                    <div class="<?= $this->prefix_kebab ?>input-item">
                         <div class="-label">
                             <label for="password">Пароль</label>
                         </div>
@@ -34,12 +32,11 @@
                             <input id="password" type="password" name="password" >
                         </div>
                     </div>
-                    <div class="input-item">
+                    <div class="<?= $this->prefix_kebab ?>input-item">
                         <div class="-label">
-
                         </div>
                         <div class="-button -forgot">
-                            <button type="submit" class="form-button">Войти</button>
+                            <button type="submit" class="-form-button">Войти</button>
                             <div class="button-message"><a href="#" data-popup-toggle="forgot-password" data-endpoint="https://www.promkod.ru/popup/forgot-password" rel="nofollow"> Забыли пароль?</a>
                             </div>
                         </div>
@@ -49,54 +46,4 @@
         </div>
     </div>
 </div>
-
-<div class="<?= $this->prefix_kebab ?>content">
-    <div class="<?= $this->prefix_kebab ?>container">
-        <div class="<?= $this->prefix_kebab ?>form-section">
-            <div class="form-content -sign">
-                <div class="input-item">
-                    <div class="-label"></div>
-                    <div class="-input">
-                        <ul class="tabs">
-                            <li class="tabs-item tabs-item-active">Войти</li>
-                            <li><a href="https://www.promkod.ru/register" class="tabs-item -links">Зарегистрироваться</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <form method="POST" action="https://www.promkod.ru/login">
-                    <input type="hidden" name="_token" value="Y4PgzoPPsP1ogoIV2jOJvq1kYWHDpxtyB0bIeI0t">
-                    <input type="hidden" name="previousUrl" value="">
-                    <div class="input-item">
-                        <div class="-label">
-                            <label for="email">Электронная почта</label>
-                        </div>
-                        <div class="-input">
-                            <input id="email" type="email" name="email" value="" >
-                            <div class="validation-error">Введите e-mail</div>
-                        </div>
-
-
-                    </div>
-                    <div class="input-item">
-                        <div class="-label">
-                            <label for="password">Пароль</label>
-                        </div>
-                        <div class="-input">
-                            <input id="password" type="password" name="password" >
-                        </div>
-                    </div>
-                    <div class="input-item">
-                        <div class="-label">
-
-                        </div>
-                        <div class="-button -forgot">
-                            <button type="submit" class="form-button">Войти</button>
-                            <div class="button-message"><a href="#" data-popup-toggle="forgot-password" data-endpoint="https://www.promkod.ru/popup/forgot-password" rel="nofollow"> Забыли пароль?</a>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
 </div>
